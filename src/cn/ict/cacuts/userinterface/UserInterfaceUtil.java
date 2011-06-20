aefb3c246a51a3439943130871e2f47b009a4206
Han Zhaoxia <hanzhaoxia@ict.ac.cn>
2011-06-20 14:28:31 +0800

    some package

---
+++ src/cn/ict/cacuts/userinterface/UserInterfaceUtil.java
@@ -0,0 +1,13 @@
+package cn.ict.cacuts.userinterface;
+
+public class UserInterfaceUtil {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("generate the framework");
+	}
+
+}
