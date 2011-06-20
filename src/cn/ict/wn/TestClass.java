17c4860bc67ce8219bf069f281a3bcb5479806a3
Wang Nan <pi3orama@gmail.com>
2011-06-20 14:19:37 +0800

    add init files

---
+++ src/cn/ict/wn/TestClass.java
@@ -0,0 +1,13 @@
+package cn.ict.wn;
+
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+	}
+
+}
